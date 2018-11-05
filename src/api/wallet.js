fc3ae41b318996fb83869b080bfeb4cd06ec7cdc
john <sz.gs.john@gmail.com>
2018-11-05 09:41:16 +0800

    change create-keypair api url

--- src/api/wallet.js
+++ src/api/wallet.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function createKeypair(query) {
     return request({
-        url: '/v1/wallet/create-keypair',
+        url: '/admin/wallet/create-keypair',
         method: 'post',
         params: query
     })
